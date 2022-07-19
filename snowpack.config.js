585c992acf103f65c3c8c91d5b663fc89cf11cb1
Leon0241 <enliang.leon.lee@gmail.com>
2022-07-19 20:24:49 +0100

    cli

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,29 +3,55 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+  // Source locations to build files from
   mount: {
-    "dist": "/",
+    // Input folder : Output Folder (Relative to buildOptions.out)
     "src": "/"
   },
+
+  // Additional plugins apart from JS/TS
   plugins: [
-    '@snowpack/plugin-sass',
+    [
+      '@snowpack/plugin-sass',
+      {
+        compilerOptions: {
+          sourceMap: true,
+          embedSourceMap: true,
+        }
+      }
+    ]
   ],
+
   packageOptions: {
     /* ... */
   },
   devOptions: {
     /* ... */
   },
+
+  // Development Server options
   devOptions: {
+    // Port to open dev server on (can always be changed)
     port: 9009
   },
+
+  // Build options 
   buildOptions: {
-    baseUrl: './',
+    // Output folder relative to root
     out: 'dist',
+    sourcemap: true,
   },
+
+  // Build optimisations
   optimize: {
+    // Bundle multiple JS files into one
     bundle: true,
+
+    // Minify the files
     minify: true,
+
+    sourcemap: 'external',
+
     treeshake: true,
     target: 'es2018',
   },
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,18 +1,20 @@
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from 'firebase/app'
+// import { getAnalytics } from "firebase/analytics";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB-TbrOOamvPc8vmpAguCvM66FvgcbCubc",
-  authDomain: "c-productions.firebaseapp.com",
-  projectId: "c-productions",
-  storageBucket: "c-productions.appspot.com",
-  messagingSenderId: "287512484062",
-  appId: "1:287512484062:web:965ed911fbe4cb63898a4f",
-  measurementId: "G-8RENRXBMJV"
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB-TbrOOamvPc8vmpAguCvM66FvgcbCubc",
+//   authDomain: "c-productions.firebaseapp.com",
+//   projectId: "c-productions",
+//   storageBucket: "c-productions.appspot.com",
+//   messagingSenderId: "287512484062",
+//   appId: "1:287512484062:web:965ed911fbe4cb63898a4f",
+//   measurementId: "G-8RENRXBMJV"
+// };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 console.log("hello world")
+
+console.log("hello word")
